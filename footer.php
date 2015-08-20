6c817611591c1cc62f8da6890883dded45136c4b
Douglas Moura <douglas.ademoura@gmail.com>
2015-08-20 19:49:22 +0200

    Added setting to insert social network links to the footer.

--- footer.php
+++ footer.php
@@ -15,6 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
+			<?php eng_social_links(); ?>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'eng' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'eng' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'eng' ), 'eng', '<a href="http://douglasmoura.com" rel="designer">Douglas Moura</a>' ); ?>
