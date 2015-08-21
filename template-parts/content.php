3e2504c8d2836b572458c558baf86f52186f58de
Douglas Moura <douglas.ademoura@gmail.com>
2015-08-21 20:54:12 +0200

    Minor fix

--- template-parts/content.php
+++ template-parts/content.php
@@ -24,23 +24,6 @@
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 	
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'eng' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'eng' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
 	<footer class="entry-footer">
 		<?php eng_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
