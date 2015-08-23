774b39847af1b7cc68b6f140662dbe258e2da6e5
Douglas Moura <douglas.ademoura@gmail.com>
2015-08-23 08:25:59 +0200

    Styles updated

--- functions.php
+++ functions.php
@@ -116,6 +116,12 @@ function eng_scripts() {
 
 	wp_enqueue_script( 'eng-scripts', get_template_directory_uri() . '/js/scripts.js', array(), '20130115', true );
 
+	if ( is_front_page()  ) {
+		wp_enqueue_script( 'slick-carousel', '//cdn.jsdelivr.net/jquery.slick/1.5.7/slick.min.js', ['jquery'], '1.5.7', true );
+		wp_enqueue_script( 'slick', get_template_directory_uri() . '/js/slick.js', array(), '20130115', true );
+		wp_enqueue_style( 'slick-style', '//cdn.jsdelivr.net/jquery.slick/1.5.7/slick.css' );
+	}
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
---
+++ js/slick.js
@@ -0,0 +1,10 @@
+jQuery('.slides').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+//  autoplay: true,
+//  autoplaySpeed: 4000
+});
+		
