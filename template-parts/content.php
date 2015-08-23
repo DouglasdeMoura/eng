6e76a2860e5f4e4d531adb3b84d1e3184e7238c5
Douglas Moura <douglas.ademoura@gmail.com>
2015-08-23 21:33:33 +0200

    Styles updated

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,9 +8,11 @@
  */
 
 ?>
-
+	
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
+	<?php eng_thumbnail(); ?>
+
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
@@ -20,9 +22,7 @@
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
-	
-	<?php eng_thumbnail(); ?>
-	
+
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
